d3e1656662827dbc6b52cdbe7a203131be972cd0
AlexMaHao <zziamahao@163.com>
2016-05-17 23:56:23 +0800

    添加ListView的弹性滚动，以及跟随ListView滑动的toolBar

--- systemwidgetdemo/src/main/java/com/mahao/alex/systemwidgetdemo/listView/ListViewActivity.java
+++ systemwidgetdemo/src/main/java/com/mahao/alex/systemwidgetdemo/listView/ListViewActivity.java
@@ -3,6 +3,8 @@ package com.mahao.alex.systemwidgetdemo.listView;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.ListView;
 
 import com.mahao.alex.systemwidgetdemo.R;
@@ -18,7 +20,7 @@ import java.util.List;
  */
 public class ListViewActivity extends AppCompatActivity {
 
-    private ListView mLv;
+    private MyListView mLv;
 
     private List<String> datas = new ArrayList<>();
 
@@ -30,13 +32,19 @@ public class ListViewActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_listview);
 
-        mLv = ((ListView) findViewById(R.id.listview_lv));
+        mLv = ((MyListView) findViewById(R.id.listview_lv));
 
         initDatas();
 
         mAdapter = new SimpleBaseAdapter(datas);
 
         mLv.setAdapter(mAdapter);
+
+        //设置空视图,调用此方法会默认隐藏Empty_view
+        mLv.setEmptyView(findViewById(R.id.listview_empty_view));
+
+        //该方法无法获取相应位置的变量
+        //Log.i("info",mLv.getChildCount()+"");
 
     }
 
@@ -46,5 +54,38 @@ public class ListViewActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * 瞬间滚动
+     * @param view
+     */
+    public void selection(View view){
+        mLv.setSelection(8);
+    }
+
+    /**
+     * 平滑滚动
+     * @param vieww
+     */
+    public void smooth(View vieww){
+        mLv.smoothScrollToPosition(1);
+    }
+
+    /**
+     *
+     * @param view
+     */
+    public void clear(View view){
+        datas.clear();
+        mAdapter.notifyDataSetChanged();
+    }
+
+    /**
+     *
+     * @param view
+     */
+    public void add(View view){
+        initDatas();
+        mAdapter.notifyDataSetChanged();
+    }
 
 }
